4f8809fd8b830e5bef0c31a6aea488bfd3655374
dtatianaperalta@gmail.com <[dtatianaperaltagmail.com[git config --global user.email [dtatianaperaltagmail.com>
2024-02-20 21:23:09 +0000

    C - Hello, world!

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
-int main()
+int main(void)
 {
-/* using puts to print "Programming is like building a multilingual puzzle */
-char str1[] = "\"Programming is like building a multilingual puzzle"; 
-puts(str1);
-return (0);
+  /* using puts to print "Programming is like building a multilingual puzzle */
+  char str1[] = "\"Programming is like building a multilingual puzzle"; 
+
+  /* e */
+  puts(str1);
+
+  return (0);
 }
