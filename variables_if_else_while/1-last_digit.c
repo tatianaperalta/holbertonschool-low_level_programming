1728a7113b9eca359323581101ad59027e30f461
root <dtatianaperalta@gmail.com>
2024-02-22 23:29:01 +0000

    C - Variables, if, else, while

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -10,27 +10,25 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-  int n, l;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	l = n%10;
-	if (l > 5)
-	  {
-	    printf("Last digit of %i is %i and is greater than 5\n", n, l);
-	  }
-	else
-	  {
-	    if (l < 6 && l != 0)
-	      {
-		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
-	      }
-	    else
-	      {
-		printf("Last digit of %i is %i and is 0\n", n, l);
-	      }
-		   
-	  }
-	return (0);
+int n, l;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+l = n % 10;
+if (l > 5)
+{
+printf("Last digit of %i is %i and is greater than 5\n", n, l);
+}
+else
+{
+if (l < 6 && l != 0)
+{
+printf("Last digit of %i is %i and is less than 6 and not 0\n", n, l);
+}
+else
+{
+printf("Last digit of %i is %i and is 0\n", n, l);
+}
+}
+return (0);
 }
