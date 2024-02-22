b8532d78353a483f6e94481a912e8a1194667639
root <dtatianaperalta@gmail.com>
2024-02-22 00:52:03 +0000

    C - Hello, world!

--- hello_world/main.s
+++ hello_world/main.s
@@ -15,3 +15,7 @@ main:
     .cfi_def_cfa 7, 8
     ret
     .cfi_endproc
+.LFE0:
+    .size   main, .-main
+    .ident  "GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.2) 5.4.0 20160609"
+    .section    .note.GNU-stack,"",@progbits
