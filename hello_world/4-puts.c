9c16fd860a203b2b2d9416537b9aba3503fdd36e
root <dtatianaperalta@gmail.com>
2024-02-22 01:40:26 +0000

    C - Hello, world!

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-/* main function */
+
+/**
+ * main - Entry point
+ * 
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 /* using puts to print "Programming is like building a multilingual puzzle */
