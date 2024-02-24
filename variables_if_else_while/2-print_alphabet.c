0c1ffadf8cc043cd50367f138e04fc6e31450038
root <dtatianaperalta@gmail.com>
2024-02-24 19:34:07 +0000

    C - Variables, if, else, while

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,13 @@
 * Return: Always 0 (Success)
 *
 */
-
 int main(void)
 {
-  char ch;
-  
-  for (ch = 'a'; ch <= 'z'; ch++)
-    {
-    putchar(ch);
-    }
-  putchar('\n');
-  return 0;
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return 0;
 }
