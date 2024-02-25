6215466613b264035996df00db43512efef4c163
root <dtatianaperalta@gmail.com>
2024-02-25 02:51:26 +0000

    C - Functions, nested loops

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+*
+* Return: On success 1.
+* 
+*/
+
+int main(void)
+{
+  putchar('_');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+  putchar('\n');
+
+  return (0);
+}
