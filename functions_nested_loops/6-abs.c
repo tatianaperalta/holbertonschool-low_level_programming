4552e484b227f95ddcfa0ce29403a7818234f697
dtatianaperalta@gmail.com <dtatianaperalta@gmail.com>
2024-02-25 04:58:49 +0000

    C - Functions, nested loops

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _abs - prints absolute values
+* @c: represents values
+* Return: Always 0 (Success)
+*
+*/
+
+int _abs(int c)
+{
+if (c < 0)
+{
+return (c);
+}
+return (-c);
+}
