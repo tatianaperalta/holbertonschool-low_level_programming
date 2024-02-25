f08ad68afcfb33cd41f28d887923354861c233f4
root <dtatianaperalta@gmail.com>
2024-02-25 00:08:17 +0000

    C - Variables, if, else, while

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,17 +8,15 @@
 
 int main(void)
 {
-  int c;
-  for (c = 0; c < 10; c++)
-    {
-      putchar((c % 10) + '0');
-      if (c == 9)
-	continue;
-      putchar(',');
-      putchar(' ');
-    }
-  putchar('\n');
-
+int c;
+for (c = 0; c < 10; c++)
+{
+putchar((c % 10) + '0');
+if (c == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
 return (0);
-
 }
