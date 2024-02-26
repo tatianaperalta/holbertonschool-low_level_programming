4dabde877b5e1e764f105c72d8bb3fb9742f833a
root <dtatianaperalta@gmail.com>
2024-02-26 01:07:34 +0000

    C - Functions, nested loops

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_last_digit - prints module
+* @n: represents values
+* @l: represents module
+* Return: Always 0 (Success)
+*
+*/
+
+int print_last_digit(int n)
+{
+int l = n % 10; 
+return l;
+}
