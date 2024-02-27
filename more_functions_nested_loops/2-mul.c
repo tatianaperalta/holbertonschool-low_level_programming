8b8e93974d7c02336339a4194ddef2196cdb97a5
root <dtatianaperalta@gmail.com>
2024-02-27 02:24:37 +0000

    2. Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - function to multiplication
+* @a: defines first parameter
+* @b: defines second parameter
+* Return: Always 0.
+*/
+
+int mul(int a, int b)
+{
+printf("%d", a * b);
+return (0);
+}
