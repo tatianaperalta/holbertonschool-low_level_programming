e5c86d1ba758e1080a880bdec061ca30513745fb
root <dtatianaperalta@gmail.com>
2024-02-27 03:10:24 +0000

    4. I believe in numbers and signs

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 
 /**
-* print_numbers - function to numbers
-* @n: defines the parameter
+* print_most_numbers - function to numbers
+*
 * Return: Always 0.
 */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 char n;
 for (n = '0'; n <= '9'; n++)
 {
+if (n != '2' && n != '4')
 _putchar(n);
 }
 _putchar('\n');
