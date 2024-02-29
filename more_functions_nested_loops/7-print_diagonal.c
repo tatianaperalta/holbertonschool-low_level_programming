03d0e1581d24ae3a180a56fbd81868d5d59cdd34
root <dtatianaperalta@gmail.com>
2024-02-29 02:51:08 +0000

    7. I feel like I am diagonally parked in a parallel universe

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -2,30 +2,30 @@
 #include "main.h"
 
 /**
-* print_line - prints lines
-* @n: variable for number of underscore
+* print_diagonal - prototype function
+* @n: variable for number of input
 * Return: Always 0 (Success)
 *
 */
 
 void print_diagonal(int n)
 {
-  int s, a;
-
-	if (n > 0)
-	{
-		for (s = 0; s < n; s++)
-		{
-		  for (a = 0; a <= s; a++)
-		    {
-			_putchar(' ');
-		    }
-		  _putchar('\\');
-		  _putchar('\n');
-		}
-	}
-	else
-	  {
-	    _putchar('\n');
-	  }
+int s;
+int a;
+if (n > 0)
+{
+for (s = 0; s < n; s++)
+{
+for (a = 0; a <= s; a++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
 }
