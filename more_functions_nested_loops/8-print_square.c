4678a49874098a1d7920c881ff93c7c74a59936f
root <dtatianaperalta@gmail.com>
2024-03-01 19:00:37 +0000

    8. You are so much sunshine in every square inch

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - prototype function
+* @size: variable for number of signs
+* Return: Always 0 (Success)
+*
+*/
+
+void print_square(int size)
+{
+int n;
+int l;
+
+if ( size > 0)
+{
+for (n = 0; n < size; n++)
+{
+for (l = 0; l < size; l++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
