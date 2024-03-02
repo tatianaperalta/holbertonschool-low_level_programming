7799d4002cf17be045dd64f00674e1053a0b4271
root <dtatianaperalta@gmail.com>
2024-03-02 00:00:31 +0000

    10. Triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_square - prototype function
+* @size: variable for number of signs
+* Return: Always 0 (Success)
+*
+*/
+
+void print_triangle(int size)
+{
+int n;
+int l;
+ 
+ if (size > 0)
+  {
+     for (l = 1; l <= size; l++)
+       {
+	 for (n = size - l; n > 0; n--)
+	   {
+	     _putchar(' ');
+	   }
+	 for (n = 0; n < l; n++)
+	   {
+	     _putchar('#');
+	   }
+	 if (l == size)
+	   continue;
+
+	 _putchar('\n');
+
+       }
+   }
+ _putchar('\n');
+}
